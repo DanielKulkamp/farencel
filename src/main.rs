8de7da9278dd4498d3f406e216eab1b8da598d64
Daniel Clemes Kulkamp <daniel.kulkamp@gmail.com>
2020-06-24 14:57:04 -0300

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_temperature = String::new();
+    let temperature : f32;
+    let mut unit = String::new();
+    loop {
+        println!("Enter the temperature");
+        input_temperature = String::new();
+        io::stdin()
+            .read_line(&mut input_temperature)
+            .expect("Failed to read line");
+
+        temperature = match input_temperature.trim().parse::<f32>() {
+            Ok(num) =>  {
+                num * 1.0
+            },
+            Err(_) => {
+                println!("You didn't enter a valid number");
+                continue;
+            }
+        };
+        break;
+    }
+    println!("You entered {}", temperature);
+
+    loop {
+        let F = String::from("F");
+        let C = String::from("C");
+
+        println!("Enter the unit ('F' or 'C')");
+        unit = String::new();
+        match io::stdin().read_line(&mut unit) {
+            Err(_) => {
+                println!("Failed to read unity line");
+                continue;
+            },
+            Ok(a) => {
+                println!("a = {}, unit = {}", a, unit);
+            }
+        };
+        unit = unit.trim().to_string();
+        println!("Unit = {}", unit);
+        if unit.eq_ignore_ascii_case(&F) {
+            println!("F");
+            println!("you entered {}°F that equals {}°C",temperature, (temperature-32.0)/1.8);
+            break;
+        }
+        if unit.eq_ignore_ascii_case(&C) {
+            println!("C");
+            println!("you entered {}°C that equals {}°F",temperature, (temperature*1.8)+32.0);
+            break;
+        }
+
+    }
 }
